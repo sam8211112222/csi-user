285cb17702b88e34a08e24a3250fe117399387ae
CHSI\si1206 <samchen@ch-si.com.tw>
2021-05-28 17:18:26 +0800

    use spring data jpa first commit

---
+++ src/main/java/com/example/demo/dao/EmployeeRepository.java
@@ -0,0 +1,14 @@
+package com.example.demo.dao;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.demo.entity.Employee;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+	// that's it ... no need to write any code LOL!
+
+	// add a method to sort by last name
+	public List<Employee> findAllByOrderByLastNameAsc();
+}
