4530da6bdf170143ac264e8d953eb98fb3a335b0
CHSI\si1206 <samchen@ch-si.com.tw>
2021-07-16 10:44:18 +0800

    暫時版本

--- src/main/resources/static/sql/user.sql
+++ src/main/resources/static/sql/user.sql
@@ -1,16 +1,9 @@
-CREATE DATABASE  IF NOT EXISTS `prototype_directory`;
-USE `prototype_directory`;
-
---
--- Table structure for table `user`
---
-
 DROP TABLE IF EXISTS `user`;
 
 CREATE TABLE `user` (
                         `id`  bigint(4) unsigned zerofill NOT NULL AUTO_INCREMENT,
                         `org_code` int(4) NOT NULL,
-                        `formal_name` varchar(45) DEFAULT NULL,
+                        `formalname` varchar(45) DEFAULT NULL,
                         `username` varchar(45) DEFAULT NULL,
                         `password` varchar(100) DEFAULT NULL,
                         `enabled` boolean DEFAULT false,
@@ -31,4 +24,4 @@ INSERT INTO `user` VALUES
 ('1004',1,'丁哥','test123','$2a$10$V3ofie/tuk83liTfQ02DOuEfg8bW8UesYNUYnw4yID1dFeIPsyK32',1,'ROLE_EMPLOYEE'),
 ('1005',2,'姚左群','test456','$2a$10$V3ofie/tuk83liTfQ02DOuEfg8bW8UesYNUYnw4yID1dFeIPsyK32',0,'ROLE_EMPLOYEE'),
 ('1006',3,'陳文政','test789','$2a$10$V3ofie/tuk83liTfQ02DOuEfg8bW8UesYNUYnw4yID1dFeIPsyK32',0,'ROLE_EMPLOYEE');
-
+ 
